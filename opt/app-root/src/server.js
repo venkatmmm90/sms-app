4a08b45ca34ba5a5e8c2e5b4774ab48f9556a9f5
venkatmmm90 <36129115+venkatmmm90@users.noreply.github.com>
2018-02-04 18:35:46 +0530

    Update server.js

--- opt/app-root/src/server.js
+++ opt/app-root/src/server.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World! Initial setup is done');
-}).listen(8080);
+}).listen(8989);

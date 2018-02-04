09d119d5dad453f4e3e09b2a474b627c0900bf3b
venkatmmm90 <36129115+venkatmmm90@users.noreply.github.com>
2018-02-04 20:38:49 +0530

    Update server.js

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
+var express = require('express'),
+    app     = express();
 
-app.get('/', (req, res) => res.send('Hello World! test'))
+app.get('/', (req, res) => res.send('Hello World! test'));
 
-app.listen(8080, () => console.log('Example app listening on port 8080!'))
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+    
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;

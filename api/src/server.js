c1b0829458db15a8a1d0d13e40b8f2c8ff64824b
Venkat <venkatmmm90@gmail.com>
2018-02-15 01:06:48 +0530

    updated

--- api/src/server.js
+++ api/src/server.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app     = express();
 var mongoose = require('mongoose');
 
-app.get('/', (req, res) => res.send('Hello World! test'));
+app.get('/testurl', (req, res) => res.send('Hello World! test'));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
@@ -12,21 +12,10 @@ console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;
 
+var templateController = require('./controllers/templateController');
+templateController(app);
 
 
-var schema  = mongoose.Schema;
-var templateSchema = new schema({
-	header:String,
-	text:String,
-	access:String,
-	isfavourite:Boolean,
-	id:String,
-	createdAt:String,
-	createdBy:String
-});
-
-var Template=mongoose.model('Template',templateSchema);
-module.exports=Template;
 
 mongoose.connect('mongodb+srv://admin:limat@cluster0-xr1wf.mongodb.net/smsapp');
 var db = mongoose.connection;
